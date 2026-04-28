f6a5f0fa454a706fcbf216c7583b009f81ecc7f8
MARCMAX Developer <dev@marcmax.ai>
2026-04-28 10:36:06 +0500

    Add X-Robots-Tag headers to prevent bots from getting stuck in JS challenges

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,20 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  // This helps prevent bots from getting stuck in a JS challenge
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'X-Robots-Tag',
+            value: 'index, follow, max-image-preview:large, max-snippet:-1, max-video-preview:-1',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
